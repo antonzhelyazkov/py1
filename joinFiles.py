f232ac352d5fabdcb4d949db17c3f4d4c0f2266a
antonzhelyazkov <antonisimo@gmail.com>
2020-09-11 23:52:25 +0300

    joinFiles debug 13

--- joinFiles.py
+++ joinFiles.py
@@ -169,6 +169,7 @@ for media in config_data['media']:
             "%d-%m-%Y_%H-%M")
         if expected_end < currentTS and item['processed'] == 'false':
             dst_dir = dst_root + "/" + issue_name
+            debug_file = dst_root + "/" + issue_name + ".list"
             if os.path.exists(dst_dir):
                 shutil.rmtree(dst_dir)
                 os.makedirs(dst_dir)
@@ -178,25 +179,26 @@ for media in config_data['media']:
             ffmpeg_output_file = dst_dir + "/output.txt"
             destination_file = dst_dir + "/" + issue_name + ".mp4"
             create_chlist(expected_start, expected_end, chunk_list_file, media)
-            # join_files(chunk_list_file, destination_file)
-            # if check_duration(destination_file, expected_start, expected_end):
-            #    print("OK", issue_name)
-            #    if ftp_upload(destination_file):
-            #        item['processed'] = 'true'
-            #        with open(path_json, 'w') as json_file:
-            #            json.dump(data, json_file)
-            #        json_file.close()
-            #        shutil.rmtree(dst_dir)
-            #        insert_mysql(media, issue_name)
-            #    else:
-            #        print("ERR problem in FTP upload", item['name'])
-            # else:
-            #    print("ERR", issue_name)
-            #    item['processed'] = 'err'
-            #    with open(path_json, 'w') as json_file:
-            #        json.dump(data, json_file)
-            #    json_file.close()
-            #    shutil.rmtree(dst_dir)
+            join_files(chunk_list_file, destination_file)
+            shutil.copyfile(chunk_list_file, debug_file)
+            if check_duration(destination_file, expected_start, expected_end):
+                print("OK", issue_name)
+                if ftp_upload(destination_file):
+                    item['processed'] = 'true'
+                    with open(path_json, 'w') as json_file:
+                        json.dump(data, json_file)
+                    json_file.close()
+                    shutil.rmtree(dst_dir)
+                    insert_mysql(media, issue_name)
+                else:
+                    print("ERR problem in FTP upload", item['name'])
+            else:
+                print("ERR", issue_name)
+                item['processed'] = 'err'
+                with open(path_json, 'w') as json_file:
+                    json.dump(data, json_file)
+                json_file.close()
+                shutil.rmtree(dst_dir)
         elif expected_end < currentTS and item['processed'] == 'true':
             print(issue_name, "Already Done", time.ctime(expected_start), time.ctime(expected_end))
         else:
