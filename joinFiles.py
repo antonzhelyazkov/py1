08ae9f7a028094a91ef42a026db750b0e01253b0
antonzhelyazkov <antonisimo@gmail.com>
2020-09-18 17:30:32 +0300

    join files 4

--- joinFiles.py
+++ joinFiles.py
@@ -82,7 +82,7 @@ def modify_stamp_to_unix_timestamp(date_stamp):
 
 
 def ftp_clear_old_files(ftp_session):
-    yesterday = datetime.today() - timedelta(days=1)
+    yesterday = datetime.today() - timedelta(days=0)
     time_yesterday = round(datetime.timestamp(yesterday))
     for name, facts in ftp_session.mlsd():
         logger.warning(f"{time_yesterday} {modify_stamp_to_unix_timestamp(facts['modify'])}")
