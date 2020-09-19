9d3c57a43f2261a72ef5ac66adcd3b40a86d888a
antonzhelyazkov <antonisimo@gmail.com>
2020-09-19 21:26:21 +0300

    delete files 1

--- joinFiles.py
+++ joinFiles.py
@@ -89,10 +89,7 @@ def ftp_clear_old_files(ftp_session):
         logger.warning(f"INFO {time_yesterday} {issue_time}")
         if int(time_yesterday) > issue_time:
             logger.warning(f"DELETE {name} {time_yesterday} {issue_time}")
-            try:
-                ftp_session.delete(name)
-            except Exception:
-                logger.warning(f"could not delete {name}")
+            ftp_session.delete(name)
 
 
 def ftp_upload(file_to_upload):
